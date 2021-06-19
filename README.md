512610d9b20a00a68216ec1b445f1f1da540566e
Ben Luo <4107322+bl1428@users.noreply.github.com>
2021-06-19 01:55:33 -0500

    Add !help, support multiple answers & responses

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 This is a Discord bot which responds to DMs with set responses. Developed & tested on Windows 10.
 
 ## Requirements
-- Node.js
+- Node.js >= 14.17.1
+
+## Configuration options
+- `botToken`: The bot's OAuth2 token
+- `missResponse`: The bot's response to incorrect answers
+- `correctAnswers`: A table (dictionary) of correct answers and the bot's corresponding responses
 
 ## Commands
 - `!dm`: Commands the bot to direct message you.
+- `!help`: Display help message.
+
+## Running the bot
+1. Open command prompt or PowerShell in this repo's root directory
+1. (First time only) Run `npm install`
+1. Navigate to `\script` directory
+1. Run with `node index.js`
--- script/index.js
+++ script/index.js
@@ -12,14 +12,34 @@ client.on('ready', () => {
 
 // Handler for incoming messages
 client.on('message', async msg => {
-    // prevent bot from responding to itself and other bots
+    // Prevent bot from responding to itself and other bots
     if (msg.author.bot) {
         return;
     }
-    // hello world
-    if (msg.content.startsWith('!hello')) {
-        msg.reply('world!');
+
+    // !dm command
+    if (msg.content.startsWith('!dm')) {
+        let dmContent = `Hi, ${msg.member.displayName}! Please enter your answer.`;
+        msg.author.send(dmContent);
+    }
+
+    // !help command
+    if (msg.content.startsWith('!help')) {
+        let messageContent = `DM me your answer!\nFor convenience, you can use \`!dm\` to command me to DM you.`;
+        msg.channel.send(messageContent);
+    }
+
+    // DM responses
+    if (msg.channel.type == 'dm') {
+        let correctAnswers = config.correctAnswers;
+        let dmContent = config.missResponse;
+
+        if (msg.content in correctAnswers) {
+            dmContent = correctAnswers[msg.content];
+        }
+        msg.reply(dmContent);
     }
 });
 
 client.login(config.botToken);
+
