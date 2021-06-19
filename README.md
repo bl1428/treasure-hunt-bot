3b6c0176ef1cdcd9609cf5d38ed7f4b190322a0a
Ben Luo <4107322+bl1428@users.noreply.github.com>
2021-06-19 00:11:52 -0500

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # treasure-hunt-bot
-This is a Discord bot which responds to DMs with set responses.
+This is a Discord bot which responds to DMs with set responses. Developed & tested on Windows 10.
+
+## Requirements
+- Node.js
+
+## Commands
+- `!dm`: Commands the bot to direct message you.
--- script/index.js
+++ script/index.js
@@ -1,3 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// parse config
+const fs = require('fs');
+const toml = require('toml');
+const config = toml.parse(fs.readFileSync('./config.toml', 'utf-8'));
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Handler for incoming messages
+client.on('message', async msg => {
+    // prevent bot from responding to itself and other bots
+    if (msg.author.bot) {
+        return;
+    }
+    // hello world
+    if (msg.content.startsWith('!hello')) {
+        msg.reply('world!');
+    }
+});
+
+client.login(config.botToken);
